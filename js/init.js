9f72193abf3f596cbb935558704cad17d21a4397
yusharnadi <yust.44@gmail.com>
2020-08-06 20:00:49 +0700

    req permision & push manager move to service workker scope

--- js/init.js
+++ js/init.js
@@ -4,6 +4,44 @@ if ("serviceWorker" in navigator) {
 			.register("/service-worker.js")
 			.then(function () {
 				console.log("Service worker registered.");
+				// REQUEST NOTIFICATION PERMISSION
+				if ("Notification" in window) {
+					Notification.requestPermission().then(function (result) {
+						if (result === "denied") {
+							console.log("Fitur notifikasi tidak diijinkan.");
+							return;
+						} else if (result === "default") {
+							console.error("Pengguna menutup kotak dialog permintaan ijin.");
+							return;
+						}
+
+						navigator.serviceWorker.getRegistration().then(function (reg) {
+							reg.showNotification("Notifikasi diijinkan!", { icon: "/images/logo.png" });
+						});
+					});
+				}
+
+				//PUSH MANAGER SUBCRIBE
+				if ("PushManager" in window) {
+					navigator.serviceWorker.getRegistration().then(function (registration) {
+						registration.pushManager
+							.subscribe({
+								userVisibleOnly: true,
+								applicationServerKey: urlBase64ToUint8Array("BEyo6NJbZAUZZY4fdXv7IeIogjDWdLjl2VI_I4ozH3vfp7Mt0jvVWwFI3QYNyiDeYZZWwbVPyiGho6fv8IpLM_0"),
+							})
+							.then(function (subscribe) {
+								console.log("Berhasil melakukan subscribe dengan endpoint: ", subscribe.endpoint);
+								console.log(
+									"Berhasil melakukan subscribe dengan p256dh key: ",
+									btoa(String.fromCharCode.apply(null, new Uint8Array(subscribe.getKey("p256dh"))))
+								);
+								console.log("Berhasil melakukan subscribe dengan auth key: ", btoa(String.fromCharCode.apply(null, new Uint8Array(subscribe.getKey("auth")))));
+							})
+							.catch(function (e) {
+								console.error("Tidak dapat melakukan subscribe ", e.message);
+							});
+					});
+				}
 			})
 			.catch(function () {
 				console.log("failed to register service worker");
